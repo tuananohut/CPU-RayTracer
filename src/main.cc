d630e010c4cca62b4fef58007a4d5bc32418cb99
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-04-05 22:11:48 +0300

    diffuse materials

--- src/main.cc
+++ src/main.cc
@@ -6,25 +6,12 @@
 #include "sphere.h"
 #include "triangle.h"
 
-color ray_color(const ray& r, const hittable& world)
-{
-  hit_record rec;
-  if (world.hit(r, interval(0, infinity), rec))
-    {
-      return 0.5 * (rec.normal + color(1, 1, 1));
-    }
-  
-  vec3 unit_direction = unit_vector(r.direction());
-  auto a = .5 * (unit_direction.y() + 1.0);
-  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.847, 0.106, 0.376);
-}
-
 int main()
 {
   hittable_list world;
 
-  world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-  // world.add(make_shared<triangle>(point3(0, 1, -1), point3(1, 0, -1), point3(-1, 0, -1)));
+  // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+  world.add(make_shared<triangle>(point3(0, 1, -1), point3(1, 0, -1), point3(-1, 0, -1)));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
   
   camera cam;
@@ -32,6 +19,7 @@ int main()
   cam.aspect_ratio = 16.0/9.0;
   cam.image_width = 400;
   cam.samples_per_pixel = 100;
-
+  cam.max_depth = 50;
+  
   cam.render(world);
 }
