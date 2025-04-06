038f28e433580266b250e7eb3be91fa205bb062a
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-04-06 22:23:08 +0300

    true lambertian reflection

--- src/main.cc
+++ src/main.cc
@@ -10,8 +10,8 @@ int main()
 {
   hittable_list world;
 
-  // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-  world.add(make_shared<triangle>(point3(0, 1, -1), point3(1, 0, -1), point3(-1, 0, -1)));
+  world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+  // world.add(make_shared<triangle>(point3(0, 1, -1), point3(1, 0, -1), point3(-1, 0, -1)));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
   
   camera cam;
