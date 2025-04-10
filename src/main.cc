8258b1d286f36256c4b53f22f10fe09534e63f6d
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-04-10 21:11:22 +0300

    Schlick's approximation

--- src/main.cc
+++ src/main.cc
@@ -12,15 +12,17 @@ int main()
   hittable_list world;
 
   auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.));
-  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  auto material_left = make_shared<dielectric>(1.0 / 1.33);
+  auto material_center = make_shared<lambertian>(color(0.549, 0.137, 0.384));
+  auto material_left = make_shared<dielectric>(1.5);
+  auto material_bubble = make_shared<dielectric>(1.0 / 1.5);
   auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.);
   
   world.add(make_shared<sphere>(point3(0., -100.5, -1.), 100., material_ground));
   world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
   world.add(make_shared<sphere>(point3(-1., 0., -1.), 0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1., 0., -1.), 0.4, material_bubble));
   world.add(make_shared<sphere>(point3(1., 0., -1.), 0.5, material_right));
- 
+  
   camera cam;
 
   cam.aspect_ratio = 16.0/9.0;
--- src/material.h
+++ src/material.h
@@ -29,7 +29,7 @@ struct lambertian: public material
 	       ray& scattered) const override 
   {
     auto scatter_direction = rec.normal + random_unit_vector();
-
+    
     // Catch degenerate scatter direction
     if (scatter_direction.near_zero())
       {
@@ -89,7 +89,7 @@ struct dielectric: public material
     bool cannot_refract = ri * sin_theta > 1.;
     vec3 direction;
 
-    if (cannot_refract)
+    if (cannot_refract || reflectance(cos_theta, ri) > random_double())
       {
 	direction = reflect(unit_direction, rec.normal); 
       }
@@ -104,7 +104,15 @@ struct dielectric: public material
   
   // Refractive index in vacuum or air, or the ratio of the material's refractive index
   // over the refractive index of the enclosing media
-  double refraction_index; 
+  double refraction_index;
+
+  static double reflectance(double cosine, double refraction_index)
+  {
+    // Use Schlick's approximation for reflectance.
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0)*std::pow((1 - cosine), 5);
+  }
 };
 
 #endif
