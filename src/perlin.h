171b238d85abc1a6c5918f8a3815a66b94f4d365
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-04-26 23:37:54 +0300

    perlin noise

--- src/perlin.h
+++ src/perlin.h
@@ -17,11 +17,30 @@ struct perlin
 
   double noise(const point3& p) const
   {
-    auto i = int(4 * p.x()) & 255;
-    auto j = int(4 * p.y()) & 255;
-    auto k = int(4 * p.z()) & 255; 
+    auto u = p.x() - std::floor(p.x());
+    auto v = p.y() - std::floor(p.y());
+    auto w = p.z() - std::floor(p.z()); 
+    
+    auto i = int(std::floor(p.x()));
+    auto j = int(std::floor(p.y()));
+    auto k = int(std::floor(p.z())); 
+    
+    double c[2][2][2];
+    
+    for (int di = 0; di < 2; di++)
+      {
+	for (int dj = 0; dj < 2; dj++)
+	  {
+	    for (int dk = 0; dk < 2; dk++)
+	      {
+		c[di][dj][dk] = randfloat[perm_x[(i + di) & 255] ^
+					  perm_y[(j + dj) & 255] ^
+					  perm_z[(k + dk) & 255]];
+	      }
+	  }
+      }
 
-    return randfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+    return trilinear_interp(c, u, v, w);
   }
 
   static const int point_count = 256;
@@ -49,6 +68,30 @@ struct perlin
 	p[i] = p[target]; 
 	p[target] = tmp;
       }
+  }
+
+  static double trilinear_interp(double c[2][2][2],
+				 double u,
+				 double v,
+				 double w)
+  {
+    auto accum = 0.;
+
+    for (int i = 0; i < 2; i++)
+      {
+	for (int j = 0; j < 2; j++)
+	  {
+	    for (int k = 0; k < 2; k++)
+	      {
+		accum += (i * u + (1 - i) * (1 - u)) 
+		       * (j * v + (1 - j) * (1 - v))
+		       * (k * w + (1 - k) * (1 - w))
+		       * c[i][j][k];
+	      }
+	  }
+      }
+
+    return accum; 
   }
 };
 
