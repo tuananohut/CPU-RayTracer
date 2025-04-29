517e0a457614da1babc5184215810d2c3ece44f6
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-04-29 10:10:41 +0300

    quadrilateral

--- src/quad.h
+++ src/quad.h
@@ -10,6 +10,10 @@ struct quad: public hittable
        const vec3& v,
        shared_ptr<material> mat)
     {
+      auto n = cross(u, v);
+      normal = unit_vector(n);
+      D = dot(normal, Q); 
+      
       set_bounding_box();
     }
 
@@ -30,13 +34,33 @@ struct quad: public hittable
 	   interval ray_t,
 	   hit_record& rec) const override
   {
-    return false; // To be implemented
+    auto denom = dot(normal, r.direction());
+
+    // No hit if the ray is parallel to the plane.
+    if (std::fabs(denom) < 1e-8)
+      return false;
+
+    // Return false if the hit point parameter t is outside the ray interval.
+    auto t = (D - dot(normal, r.origin())) / denom;
+    if (!ray_t.contains(t))
+      return false;
+
+    auto intersection = r.at(t);
+
+    rec.t = t;
+    rec.p = intersection;
+    rec.mat = mat;
+    rec.set_face_normal(r, normal);
+    
+    return true; 
   }
 
   point3 Q;
   vec3 u, v;
   shared_ptr<material> mat;
-  aabb bbox; 
+  aabb bbox;
+  vec3 normal;
+  double D; 
 }
 
 #endif
