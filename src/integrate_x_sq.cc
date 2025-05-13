75718cce5b9c6f941a6815ebd4ff30f839c500de
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-05-13 21:52:04 +0300

    pdf

--- src/integrate_x_sq.cc
+++ src/integrate_x_sq.cc
@@ -3,19 +3,27 @@
 #include <iostream>
 #include <iomanip>
 
+double icd(double d)
+{
+  return 2.0 * d;
+}
+
+double pdf(double x)
+{
+  return 0.5;
+}
+
 int main()
 {
-  int a = 0;
-  int b = 2;
   int N = 1000000;
   auto sum = 0.;
 
   for (int = 0; i < N; i++)
     {
-      auto x = random_double(a, b);
-      sum += x*x; 
+      auto x = icd(random_double());
+      sum += x*x / pdf(x);  
     }
 
   std::cout << std::fixed << std::setprecision(12);
-  std::cout << "I = " << (b - a) * (sum / N) << '\n';
+  std::cout << "I = " << (sum / N) << '\n';
 }
