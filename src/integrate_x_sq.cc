2d46eb164024f2a76283222809b2f1af3669bb47
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-05-16 22:59:32 +0300

    importance sampling

--- src/integrate_x_sq.cc
+++ src/integrate_x_sq.cc
@@ -17,13 +17,14 @@ int main()
 {
   int N = 1;
   auto sum = 0.;
-
-  for (int = 0; i < N; i++)
+  
+  for (int i = 0; i < N; i++)
     {
       auto z = random_double();
       if (z == 0.0) // Ignore zero to avoid NaNs
 	continue;
-      
+
+      auto x = icd(z); 
       sum += x*x / pdf(x);  
     }
 
