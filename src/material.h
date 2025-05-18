c1e07a26d928de8e725bd5f8326b58aa9d71b077
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-05-18 23:13:00 +0300

    random hemispherical sampling

--- src/material.h
+++ src/material.h
@@ -42,7 +42,7 @@ struct lambertian: public material
 	       color& attenuation,
 	       ray& scattered) const override 
   {
-    auto scatter_direction = rec.normal + random_unit_vector();
+    auto scatter_direction = random_on_hemisphere(rec.normal);
     
     // Catch degenerate scatter direction
     if (scatter_direction.near_zero())
@@ -62,8 +62,7 @@ struct lambertian: public material
 			const hit_record& rec,
 			const ray& scattered) const override
   {
-    auto cos_theta = dot(rec.normal, unit_vector(scattered.direction()));
-    return cos_theta < 0 ? 0 : cos_theta/pi;
+    return 1 / (2*pi); 
   }
 
   shared_ptr<texture> tex; 
