7c088afd6f9b4a83b829c6b5b24bb61090b97eba
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-05-25 23:09:45 +0300

    light sampling

--- src/main.cc
+++ src/main.cc
@@ -259,7 +259,7 @@ void cornell_box()
 
   cam.aspect_ratio = 1.;
   cam.image_width = 600;
-  cam.samples_per_pixel = 1000;
+  cam.samples_per_pixel = 10;
   cam.max_depth = 50;
   cam.background = color(0, 0, 0);
 
