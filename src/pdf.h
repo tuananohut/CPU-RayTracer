b162fc4077d8d84bea8c96e2acb7a727c0c76aed
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-05-28 23:21:45 +0300

    cosine density

--- src/pdf.h
+++ src/pdf.h
@@ -5,7 +5,7 @@
 
 struct pdf
 {
-  virtual ~pdf();
+  virtual ~pdf() {}
 
   virtual double value(const vec3& direction) const = 0;
   virtual vec3 generate() const = 0;
@@ -24,6 +24,25 @@ struct sphere_pdf: public pdf
   {
     return random_unit_vector();
   }
+};
+
+struct cosine_pdf: public pdf
+{
+  cosine_pdf(const vec3& w):
+    uvw(w) {}
+
+  double value(const vec3& direction) const override
+  {
+    auto cosine_theta = dot(unit_vector(direction), uvw.w());
+    return std::fmax(0, cosine_theta/pi); 
+  }
+
+  vec3 generate() const override
+  {
+    return uvw.transform(random_cosine_direction()); 
+  }
+
+  onb uvw; 
 };
 
 #endif
