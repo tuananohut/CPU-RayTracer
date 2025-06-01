b76c33f9fb5d06554173ae88f3d6bc85d66097c6
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-06-01 23:19:45 +0300

    mixture densities

--- src/quad.h
+++ src/quad.h
@@ -18,6 +18,8 @@ struct quad: public hittable
       normal = unit_vector(n);
       D = dot(normal, Q); 
       w = n / dot(n, n); 
+
+      area = n.length();
       
       set_bounding_box();
     }
@@ -87,6 +89,24 @@ struct quad: public hittable
 
     return true; 
   }
+
+  double pdf_value(const point3& origin, const vec3& direction) const override
+  {
+    hit_record rec;
+    if (!this->hit(ray(origin, direction), interval(0.001, infinity), rec))
+      return 0;
+
+    auto distance_squared = rec.t * rec.t * direction.length_squared();
+    auto cosine = std::fabs(dot(direction, rec.normal) / direction.length());
+
+    return distance_squared / (cosine * area); 
+  }
+
+  vec3 random(const point3& origin) const override
+  {
+    auto p = Q + (random_double() * u) + (random_double() * v);
+    return p - origin; 
+  }
   
   point3 Q;
   vec3 u, v;
@@ -94,7 +114,8 @@ struct quad: public hittable
   shared_ptr<material> mat;
   aabb bbox;
   vec3 normal;
-  double D; 
+  double D;
+  double area; 
 };
 
 inline shared_ptr<hittable_list> box(const point3& a,
