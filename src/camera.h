22f4621822b612fca9c6c578b3e0a78442ab77a7
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-06-02 23:32:26 +0300

    hittable

--- src/camera.h
+++ src/camera.h
@@ -21,7 +21,7 @@ struct camera
   double defocus_angle = 0; // Variation angle of rays through each pixel
   double focus_dist = 10; // Distance from camera lookfrom point to plane of perfect focus
   
-  void render(const hittable& world)
+  void render(const hittable& world, const hittable& lights)
   {
     initialize();
 
@@ -38,7 +38,7 @@ struct camera
 		for (int s_i = 0; s_i < sqrt_spp; s_i++)
 		  {
 		    ray r = get_ray(i, j, s_i, s_j);
-		    pixel_color += ray_color(r, max_depth, world);
+		    pixel_color += ray_color(r, max_depth, world, lights);
 		  }
 	      }
 	    
@@ -138,7 +138,7 @@ struct camera
     return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v); 
   }
 
-  color ray_color(const ray& r, int depth, const hittable& world) const
+  color ray_color(const ray& r, int depth, const hittable& world, const hittable& lights) const
   {
     // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0)
@@ -160,15 +160,15 @@ struct camera
     if (!rec.mat->scatter(r, rec, attenuation, scattered, pdf_value))
       return color_from_emission; 
 
-    cosine_pdf surface_pdf(rec.normal);
-    scattered = ray(rec.p, surface_pdf.generate(), r.time());
-    pdf_value = surface_pdf.value(scattered.direction()); 
+    hittable_pdf light_pdf(lights, rec.p);
+    scattered = ray(rec.p, light_pdf.generate(), r.time());
+    pdf_value = light_pdf.value(scattered.direction());
     
     double scattering_pdf = rec.mat->scattering_pdf(r, rec, scattered);
       
-    color color_from_scatter =
-      (attenuation * scattering_pdf * ray_color(scattered, depth-1, world)) / pdf_value;
-    
+    color sample_color = ray_color(scattered, depth-1, world, lights);
+    color color_from_scatter = (attenuation * scattering_pdf * sample_color) / pdf_value;
+  
     return color_from_emission + color_from_scatter;
   }
 };
