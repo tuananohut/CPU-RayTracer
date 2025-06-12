788e55612ac603b491618804e55c75240224ed37
Tuana Nohut <tkanesrkajol58@gmail.com>
2025-06-12 23:24:23 +0300

    glass sphere

--- src/main.cc
+++ src/main.cc
@@ -244,17 +244,16 @@ void cornell_box()
   world.add(make_shared<quad>(point3(0, 0, 0), vec3(555, 0, 0), vec3(0, 0, 555), white));
   world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
   world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
+  
+  world.add(make_shared<quad>(point3(213,554,227), vec3(130,0,0), vec3(0,0,105), light));
 
-  shared_ptr<material> aluminum = make_shared<metal>(color(0.8, 0.85, 0.88), 0.); 
-  shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), aluminum);
+  shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
   box1 = make_shared<rotate_y>(box1, 15);
   box1 = make_shared<translate>(box1, vec3(265,0,295));
   world.add(box1);
 
-  shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
-  box2 = make_shared<rotate_y>(box2, -18);
-  box2 = make_shared<translate>(box2, vec3(130,0,65));
-  world.add(box2);
+  auto glass = make_shared<dielectric>(1.5);
+  world.add(make_shared<sphere>(point3(190, 90, 190), 90, glass));
 
   auto empty_material = shared_ptr<material>();
   quad lights(point3(343, 554, 332), vec3(-130, 0, 0), vec3(0, 0, -105), empty_material);
